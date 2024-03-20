d0bac637e45e90088fbc231741147f28df4458ec
bakradzetamar <156605663+bakradzetamar@users.noreply.github.com>
2024-03-20 20:52:06 +0400

    me-2

--- script.js
+++ script.js
@@ -75,28 +75,6 @@ thumbnails.forEach((thumbnail, index) => {
 
 // login
 
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.icon-close');
-
-
-registerLink.addEventListener('click', () => {
-    wrapper.classList.add('active');
-});
-
-loginLink.addEventListener('click', () => {
-    wrapper.classList.remove('active');
-});
-
-btnPopup.addEventListener('click', () => {
-    wrapper.classList.add('active-popup');
-});
-
-iconClose.addEventListener('click', () => {
-    wrapper.classList.remove('active-popup');
-});
 
 
 // oas1
